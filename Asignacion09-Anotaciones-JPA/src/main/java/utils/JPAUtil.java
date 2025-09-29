49b5695260e1174ed6b4c501a96dee54c9c43b3f
mcortezv <mdjesuscv@gmail.com>
2025-09-28 22:37:35 -0700

    JPAUtil

--- Asignacion09-Anotaciones-JPA/src/main/java/utils/JPAUtil.java
+++ Asignacion09-Anotaciones-JPA/src/main/java/utils/JPAUtil.java
@@ -1,14 +1,33 @@
-package utils;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package utils;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 /**
  *
  * @author Cotez, Manuel;
  */
 public class JPAUtil {
-    
+
+    private static final String PERSISTENCE_UNIT_NAME = "EmpresaPU";
+    private static EntityManagerFactory emf;
+
+    private JPAUtil() {}
+
+    public static EntityManager getEntityManager() {
+        if (emf == null) {
+            emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        }
+        return emf.createEntityManager();
+    }
+
+    public static void close() {
+        if (emf != null && emf.isOpen()) {
+            emf.close();
+        }
+    }
 }
