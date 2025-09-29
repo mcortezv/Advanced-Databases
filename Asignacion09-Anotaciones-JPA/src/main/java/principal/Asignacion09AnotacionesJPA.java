249e3c6735cf3b69abbc69d7d2f72a2b578abb27
mcortezv <mdjesuscv@gmail.com>
2025-09-28 22:27:54 -0700

    Creacion del Proyecto y Unidad de Persistencia

---
+++ Asignacion09-Anotaciones-JPA/src/main/java/principal/Asignacion09AnotacionesJPA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package principal;
+
+/**
+ *
+ * @author mdjes
+ */
+public class Asignacion09AnotacionesJPA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
