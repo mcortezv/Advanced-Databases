4b9a12b27b52127bb9622e32bcd71e134e7704ce
mcortezv <mdjesuscv@gmail.com>
2025-09-28 22:43:29 -0700

    Pruebas

--- Asignacion09-Anotaciones-JPA/src/main/java/main/JPAEmpresa.java
+++ Asignacion09-Anotaciones-JPA/src/main/java/main/JPAEmpresa.java
@@ -1,8 +1,11 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package main;
+import models.EmpleadoDAO;
+import models.Empleado;
+import models.Empleado.Estatus;
+import java.time.LocalDate;
 
 /**
  *
@@ -11,6 +14,47 @@ package main;
 public class JPAEmpresa {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EmpleadoDAO dao = new EmpleadoDAO();
+
+        // Insertar empleados
+        Empleado empleado1 = new Empleado("Manuel Cortez", "manuel.cortez@gmail.com",
+                15000.0, Estatus.ACTIVO, LocalDate.of(2023, 5, 10));
+        Empleado empleado2 = new Empleado("Sebastian Escalante", "sebastian.escalante@gmail.com",
+                20000.0, Estatus.ACTIVO, LocalDate.of(2022, 3, 15));
+
+        dao.insertar(empleado1);
+        dao.insertar(empleado2);
+
+        // Listar empleados
+        System.out.println("\n--- Lista de empleados ---");
+        for (Empleado empleado : dao.listar()) {
+            System.out.println(empleado);
+        }
+
+        // Modificar empleado
+        empleado1.setSalario(16000.0);
+        dao.actualizar(empleado1);
+
+        System.out.println("\n--- Despues de actualizar ---");
+        for (Empleado empleado : dao.listar()) {
+            System.out.println(empleado);
+        }
+
+        // Aumentar salario
+        dao.aumentarSalario(empleado2.getId(), 10.0);
+
+        System.out.println("\n--- Despues de aumento de salario ---");
+        for (Empleado empleado : dao.listar()) {
+            System.out.println(empleado);
+        }
+
+        // Eliminar empleado
+        dao.eliminar(empleado2.getId());
+
+        System.out.println("\n--- Despues de eliminar ---");
+        for (Empleado empleado : dao.listar()) {
+            System.out.println(empleado);
+        }
     }
 }
+
