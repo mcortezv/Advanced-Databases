a0e3e1625da2dff19bd301622d234fd3e50fcf3b
mcortezv <mdjesuscv@gmail.com>
2025-10-07 20:04:00 -0700

    Creacion del Proyecto VideojuegosJPA

---
+++ VideojuegosJPA/src/main/java/main/VideojuegosJPA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+/**
+ *
+ * @author Cortez, Manuel;
+ */
+public class VideojuegosJPA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
